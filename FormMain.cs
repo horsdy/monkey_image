f16f65eff9bc2989ecdff85a7b76939006612cef
houshidi <houshidi@sina.com>
2023-09-20 23:22:23 +0800

    feat:show date on jpg file

--- FormMain.cs
+++ FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -83,8 +84,50 @@ namespace monkey_image
 
         private bool handleJpg(string file)
         {
+            string datetime = DateTime.Now.ToString("yyyy/MM/dd");
             Console.WriteLine("handleJpg");
 
+            Bitmap fileBitMap = new Bitmap(file);
+            var prop = fileBitMap.GetPropertyItem(0x9003);
+            if (prop != null) {
+                datetime = Encoding.Default.GetString(prop.Value).Trim('\0');
+                Console.WriteLine("{0} datetime: {1}", file, datetime);
+                datetime = datetime.Split(' ')[0].Replace(':', '/');
+            }
+
+            // 设置字体和颜色
+            Font font = new Font("Arial", 80, FontStyle.Bold);
+            SolidBrush brush = new SolidBrush(Color.Yellow);
+
+            // 获取图像宽度和高度
+            int width = fileBitMap.Width;
+            int height = fileBitMap.Height;
+
+            // 计算元数据区域的宽度和高度
+            int metaWidth = width / 2;
+            int metaHeight = height / 10;
+
+            // 创建元数据区域
+            Bitmap metaBitmap = new Bitmap(metaWidth, metaHeight);
+
+            // 在元数据区域中绘制文本框
+            Graphics metaGraph = Graphics.FromImage(metaBitmap);
+            metaGraph.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            metaGraph.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            metaGraph.DrawString(datetime, font, brush, new Rectangle(0, 0, metaWidth, metaHeight));
+
+            // 将元数据区域合并到主图像上
+            Graphics fileGraph = Graphics.FromImage(fileBitMap);
+            fileGraph.DrawImage(metaBitmap, new Rectangle(width/16, (int)(height *0.95), metaWidth, metaHeight));
+
+            // 保存修改后的图像到磁盘
+            string outputPath = Path.Combine(Path.GetDirectoryName(file), "Output_" + Path.GetFileNameWithoutExtension(file) + ".jpg");
+            fileBitMap.Save(outputPath);
+
+            metaGraph.Dispose();
+            fileGraph.Dispose();
+            metaBitmap.Dispose();
+            fileBitMap.Dispose();
             return true;
         }
 
